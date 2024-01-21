ce076acc9c275850164570edf9174b3be39bad2a
krish736 <112505529+krish736@users.noreply.github.com>
2024-01-21 18:39:13 +0530

    commit

---
+++ contains_duplicate.cpp
@@ -0,0 +1,16 @@
+// Leetcode
+// 21-jan-2024
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num) > 0)
+                return true;
+            seen.insert(num);
+        }
+        return false;
+    }
+};
