d7b47fd5b86a35982d1652609bf93b02c3c65bef
krish736 <112505529+krish736@users.noreply.github.com>
2024-01-23 13:29:04 +0530

    commit

---
+++ largest_subarray_0_sum.cpp
@@ -0,0 +1,59 @@
+Gfg
+22-jan-2024
+Given an array having both positive and negative integers. The task is to compute the length of the largest subarray with sum 0.
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution{
+    public:
+    int maxLen(vector<int>&A, int n)
+    {   
+        // Your code here
+        unordered_map<int,int>mp;
+        int ans=0,sum=0;
+        for(int i=0;i<n;i++){
+            sum+=A[i];
+            if(sum==0) ans=i+1;
+            else if(mp.find(sum)!=mp.end() ){
+                ans=max(ans,i-mp[sum]);
+            }
+            else {
+                mp[sum]=i;
+            }
+        }
+        return ans;    
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        Solution ob;
+        cout<<ob.maxLen(array1,m)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
