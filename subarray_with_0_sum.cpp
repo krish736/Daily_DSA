10cba5fdfc97dce411ba66a49c184febb26cf058
krish736 <112505529+krish736@users.noreply.github.com>
2024-01-24 19:16:43 +0530

    commit

---
+++ subarray_with_0_sum.cpp
@@ -0,0 +1,57 @@
+// Gfg
+// 24-jan-2024
+// Given an array of integers. Find if there is a subarray (of size at-least one) with 0 sum. You just need to return true/false depending upon whether there is a subarray present with 0-sum or not. Printing will be taken care by the driver code.
+
+//{ Driver Code Starts
+// A C++ program to find if there is a zero sum
+// subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    //Function to check whether there is a subarray present with 0-sum or not.
+    bool subArrayExists(int arr[], int n)
+    {
+        //Your code here
+        unordered_set<int>m;
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum +=arr[i];
+            if(m.find(sum)!=m.end() || sum==0)
+            {
+                return true;
+            }
+            m.insert(sum);
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    Solution obj;
+	    	if (obj.subArrayExists(arr, n))
+		cout << "Yes\n";
+	else
+		cout << "No\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
