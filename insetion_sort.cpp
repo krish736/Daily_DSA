b9bc2c987822a071aa9a5534678af08457ce71c5
krish736 <112505529+krish736@users.noreply.github.com>
2024-03-18 10:41:06 +0530

    18-03-2024

---
+++ insetion_sort.cpp
@@ -0,0 +1,68 @@
+// 10-03-2024
+// Gfg
+// The task is to complete the insert() function which is used to implement Insertion Sort. 
+//{ Driver Code Starts
+// C program for insertion sort
+#include <stdio.h>
+#include <math.h>
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    void insert(int arr[], int i)
+    {
+        //code here
+    }
+     public:
+    //Function to sort the array using insertion sort algorithm.
+    void insertionSort(int arr[], int n)
+    {
+        //code here
+        for (int i = 1; i < n; i++){
+            for (int j = i; j > 0; j--){
+                if (arr[j] < arr[j - 1]){
+                    int temp;
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int arr[1000],n,T,i;
+
+    scanf("%d",&T);
+
+    while(T--){
+
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+
+    Solution ob;
+    ob.insertionSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
