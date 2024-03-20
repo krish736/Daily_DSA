94e1ac865f3c79767b4beff5b843ffe931f51378
krish736 <112505529+krish736@users.noreply.github.com>
2024-03-20 18:16:56 +0530

    20-03-2024

--- merge_intervals.cpp
+++ merge_intervals.cpp
@@ -1,5 +1,5 @@
 // 20-03-2024
-// Leetcode
+// Leetcode AND Gfg
 // Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
 vector<vector<int>> ans;
