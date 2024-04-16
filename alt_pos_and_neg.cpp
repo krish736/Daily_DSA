1346c5924d594d400af19f60fbdefe7011e95e20
krish736 <112505529+krish736@users.noreply.github.com>
2024-04-16 22:27:38 +0530

    commit

---
+++ alt_pos_and_neg.cpp
@@ -0,0 +1,76 @@
+// 16-04-2024
+// GFG
+// Given an unsorted array Arr of N positive and negative numbers. Your task is to create an array of alternate positive and negative numbers without changing the relative order of positive and negative numbers.
+// Note: Array should start with a positive number and 0 (zero) should be considered a positive element.
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution
+{
+public:
+    void rearrange(int arr[], int n)
+    {
+        // code here
+        vector<int> positive, negative;
+
+        for (int i = 0; i < n; ++i)
+        {
+            if (arr[i] >= 0)
+                positive.push_back(arr[i]);
+            else
+                negative.push_back(arr[i]);
+        }
+
+        int i = 0, j = 0, k = 0;
+        bool positiveTurn = true;
+
+        while (i < positive.size() && j < negative.size())
+        {
+            if (positiveTurn)
+                arr[k++] = positive[i++];
+            else
+                arr[k++] = negative[j++];
+
+            positiveTurn = !positiveTurn;
+        }
+
+        while (i < positive.size())
+            arr[k++] = positive[i++];
+
+        while (j < negative.size())
+            arr[k++] = negative[j++];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.rearrange(arr, n);
+        for (i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
---
+++ contains_duplicate_2.cpp
@@ -0,0 +1,24 @@
+// 16-04-2024
+// Leetcode
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k
+
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (mp.count(nums[i]))
+            {
+                if (abs(i - mp[nums[i]]) <= k)
+                    return true;
+            }
+            mp[nums[i]] = i;
+        }
+        return false;
+    }
+};
