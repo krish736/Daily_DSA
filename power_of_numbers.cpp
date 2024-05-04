b0bb55807c7af129ca61c7da98eb0da41a5411f2
krish736 <112505529+krish736@users.noreply.github.com>
2024-05-04 15:06:43 +0530

    04-05-2024

---
+++ power_of_numbers.cpp
@@ -0,0 +1,69 @@
+// 04-05-2024
+// Gfg
+// Given a number and its reverse. Find that number raised to the power of its own reverse.
+// Note: As answers can be very large, print the result modulo 109 + 7.
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    //You need to complete this fucntion
+    int mod=1000000007;
+    long long power(int N,int R)
+    {
+       //Your code here
+       if(R==1) return N;
+       if(R==0) return 1;
+      
+       long long val=power(N,R/2)%mod;
+       val=(val*val)%mod;
+      
+       if(R&1) val=(val*N)%mod;
+      
+       return val;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+// compute reverse of a number 
+long long rev(long long n)
+{
+    long long rev_num = 0;
+     while(n > 0) 
+      { 
+        rev_num = rev_num*10 + n%10; 
+        n = n/10; 
+      } 
+      return rev_num;
+}
+
+
+
+
+int main()
+{
+    int T;
+    cin>>T;//testcases
+    
+    while(T--)
+    {
+        long long N;
+        cin>>N;//input N
+        
+        long long R = 0; 
+        
+        // reverse the given number n
+        R = rev(N);
+        Solution ob;
+        //power of the number to it's reverse
+        long long ans =ob.power(N,R);
+        cout << ans<<endl;
+    }
+}
+// } Driver Code Ends
